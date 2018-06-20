358cc6fa94d92c8366372b212bd4f06805eb629f
x <xeniafiorenza@will-san-the-science-man.local>
2018-06-20 10:07:05 +0800

    added Category.swift

---
+++ To-Do List ™/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  To-Do List ™
+//
+//  Created by x on 20/6/18.
+//  Copyright © 2018 Beep Boop. All rights reserved.
+//
+
+import Foundation
+
+struct Category: Codable {
+    var name: String
+    var colorRed: Int
+    var colorGreen: Int
+    var colorBlue: Int
+}
